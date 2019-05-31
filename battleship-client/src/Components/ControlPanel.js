e6f54468f072565e3bda96e407639df6915ce29d
DariuszDyrda <darek.d.az.nie@interia.pl>
2019-06-01 01:42:29 +0200

    client displays total shots taken

--- battleship-client/src/Components/ControlPanel.js
+++ battleship-client/src/Components/ControlPanel.js
@@ -6,6 +6,7 @@ export default function ControlPanel(props) {
         <div className="controlPanel">
             <button>New game</button>
             <h1>Targets left: {props.gameStats.targetsLeft}</h1>
+            <h1>Shots taken: {props.gameStats.shotsTaken}</h1>
         </div>
     )
 }
